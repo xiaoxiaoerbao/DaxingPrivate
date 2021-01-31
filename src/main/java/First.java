f2b32bc83f62f0a74da597a2193476da177f654b
xiaoxiaoerbao <xulaixing2011@163.com>
2021-01-31 17:46:09 +0800

    add third jar

--- src/main/java/First.java
+++ src/main/java/First.java
@@ -1,6 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.stat.inference.TestUtils;
+import org.apache.commons.math3.stat.inference.WilcoxonSignedRankTest;
+
 public class First {
 
     public static void main(String[] args) {
-        System.out.println("ok");
+        NormalDistribution normalDistribution=new NormalDistribution();
     }
 }
--- src/test/java/First.java
+++
@@ -1,7 +0,0 @@
-public class First {
-
-    public static void main(String[] args) {
-        String a="5";
-        assert a !=null : "ok";
-    }
-}

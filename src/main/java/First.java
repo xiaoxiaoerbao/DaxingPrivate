b5de83fb0aa25facf1d95aa1073759d5c904d7f4
xiaoxiaoerbao <xulaixing2011@163.com>
2021-02-01 18:17:18 +0800

    add module DaxingPrivate

--- src/main/java/First.java
+++ src/main/java/First.java
@@ -1,6 +1,9 @@
 public class First {
 
     public static void main(String[] args) {
-//        System.out.println("ok");
+        System.out.println("ok");
+        String a=null;
+        assert a!=null : "null";
+        System.out.println(a);
     }
 }

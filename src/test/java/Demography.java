783e66a7ee6eeb2b33d9464e32139378a181182d
xiaoxiaoerbao <Xudaxing@2018>
2021-04-04 13:23:56 +0800

    Test

--- src/test/java/Demography.java
+++ src/test/java/Demography.java
@@ -3,6 +3,8 @@
 public class Demography {
 
     public static void main(String[] args) {
-        //
+        Ploidy ploidy=Ploidy.DIPLOID;
+        Ploidy ploidy1=Ploidy.TETRAPLOID;
+        Ploidy ploidy2=Ploidy.HEXAPLOID;
     }
 }
